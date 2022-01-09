d0a953b6ffa2f2f949edb234807a3c5353b5f1ea
Kevin An <kevinchongan@gmail.com>
2022-01-08 22:42:53 -0500

    [wordle-solver] Refactored code and some comments.

--- src/wordle.cpp
+++ src/wordle.cpp
@@ -12,174 +12,221 @@
 
 using namespace std;
 
-namespace {
+namespace
+{
 
     constexpr int kCandidateSizeToPrintAll = 15;
     constexpr string_view kAllCorrectResult = "22222";
 
-    bool IsLetterAllowed(int bitmask, char chr) {
+    bool IsLetterAllowed(int bitmask, char chr)
+    {
         return !(bitmask >> (chr - 'a') & 1);
     }
 
-    void SetLetterNotAllowed(int& bitmask, char chr) {
+    void SetLetterNotAllowed(int &bitmask, char chr)
+    {
         bitmask |= 1UL << (chr - 'a');
     }
 
 }
 
-bool Wordle::ValidConstraints(string_view guess, string_view result) {
+bool Wordle::ValidConstraints(string_view guess, string_view result)
+{
     bool ok = true;
     // Length check.
     ok &= (guess.size() == length_ && result.size() == length_);
     // Guess check.
-    for (const char chr : guess) {
-        if (!('a' <= chr && chr <= 'z')) {
+    for (const char chr : guess)
+    {
+        if (!('a' <= chr && chr <= 'z'))
+        {
             ok = false;
             break;
         }
     }
     // Result check.
-    for (const char chr : result) {
+    for (const char chr : result)
+    {
         int state = chr - '0';
-        if (!(0 <= state && state <= 2)) {
+        if (!(0 <= state && state <= 2))
+        {
             ok = false;
             break;
         }
     }
-    if (!ok) {
+    if (!ok)
+    {
         cout << "Inputs were malformed, please guess again." << endl;
     }
     return ok;
 }
 
-bool Wordle::FitsConstraints(string_view word) {
+bool Wordle::FitsConstraints(string_view word)
+{
     int32_t index = 0;
-    for (const auto& chr : word) {
-        if (!IsLetterAllowed(constraints_.letters[index++], chr)) {
+    for (const auto &chr : word)
+    {
+        if (!IsLetterAllowed(constraints_.letters[index++], chr))
+        {
             return false;
         }
     }
-    for (const auto& chr : constraints_.must_contain) {
-        if (word.find(chr) == string::npos) {
+    for (const auto &chr : constraints_.must_contain)
+    {
+        if (word.find(chr) == string::npos)
+        {
             return false;
         }
     }
     return true;
 }
 
-Wordle::Wordle(int word_length) {
+Wordle::Wordle(int word_length)
+{
     length_ = word_length;
 
     constraints_.letters.reserve(length_);
-    for (int i = 0; i < length_; ++i) {
+    for (int i = 0; i < length_; ++i)
+    {
         constraints_.letters.push_back(0);
     }
-    
+
     ifstream infile("word_bank\\" + to_string(length_) + ".words");
     string word;
-    while (infile >> word) {
+    while (infile >> word)
+    {
         assert(int(word.size()) == length_);
         transform(word.begin(), word.end(), word.begin(), ::tolower);
-        candidates_.insert(word);
+        candidates_.insert({word, true});
     }
 }
 
-void Wordle::Solve() {
+void Wordle::Solve()
+{
     string guess, result;
-    
+
     SolveOnce();
-    while (true) {
-        do {
+    while (true)
+    {
+        do
+        {
             cout << "Input guess: ";
             cin >> guess;
             cout << "Input result: ";
             cin >> result;
         } while (!ValidConstraints(guess, result));
-        if (result == kAllCorrectResult) {
+        if (result == kAllCorrectResult)
+        {
             cout << "Done! Congrats :)" << endl;
             break;
-        } else {
+        }
+        else
+        {
             UpdateConstraints(guess, result);
             SolveOnce();
         }
     }
 }
 
-void Wordle::UpdateConstraints(string_view guess, string_view result) {
-    for (int i = 0; i < length_; ++i) {
+void Wordle::UpdateConstraints(string_view guess, string_view result)
+{
+    for (int i = 0; i < length_; ++i)
+    {
         char chr = guess[i];
         State state = static_cast<State>(result[i] - '0');
-        switch (state) {
-            case State::kNone:
-                for (int j = 0; j < length_; ++j) {
-                    SetLetterNotAllowed(constraints_.letters[j], chr);
+        switch (state)
+        {
+        case State::kNone:
+            for (int j = 0; j < length_; ++j)
+            {
+                SetLetterNotAllowed(constraints_.letters[j], chr);
+            }
+            break;
+        case State::kAppears:
+            SetLetterNotAllowed(constraints_.letters[i], chr);
+            constraints_.must_contain.insert(chr);
+            break;
+        case State::kCorrect:
+            for (char c = 'a'; c <= 'z'; ++c)
+            {
+                if (c == chr)
+                {
+                    continue;
                 }
-                break;
-            case State::kAppears:
-                SetLetterNotAllowed(constraints_.letters[i], chr);
-                constraints_.must_contain.insert(chr);
-                break;
-            case State::kCorrect:
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    if (c == chr) {
-                        continue;
-                    }
-                    SetLetterNotAllowed(constraints_.letters[i], c);
-                }
-                constraints_.must_contain.insert(chr);
-                break;
-            default:
-                cout << "Should not happen" << endl;
+                SetLetterNotAllowed(constraints_.letters[i], c);
+            }
+            constraints_.must_contain.insert(chr);
+            break;
+        default:
+            cout << "Should not happen" << endl;
         }
     }
-    cout << "logging constraints" << endl;
-    for (char must : constraints_.must_contain) {
-        cout << must << endl;
-    }
-    for (int bitmask : constraints_.letters) {
-        cout << bitmask << endl;
+    // cout << "logging constraints" << endl;
+    // for (char must : constraints_.must_contain)
+    // {
+    //     cout << must << endl;
+    // }
+    // for (int bitmask : constraints_.letters)
+    // {
+    //     cout << bitmask << endl;
+    // }
+}
+
+void Wordle::UpdateCandidates()
+{
+    for (auto iter = candidates_.begin(), last = candidates_.end(); iter != last; ++iter)
+    {
+        if (!FitsConstraints(iter->first))
+        {
+            iter->second = false;
+        }
     }
 }
 
-void Wordle::PurgeCandidates() {
-    for (auto iter = candidates_.begin(), last = candidates_.end(); iter != last;) {
-        if (!FitsConstraints(*iter)) {
-            iter = candidates_.erase(iter);
-        } else {
-            ++iter;
-        }
-    }
-}
-
-string Wordle::GetBestGuess() {
+string Wordle::GetBestGuess()
+{
     vector<vector<int>> letter_count;
-    for (int i=0; i < length_; ++i) {
+    for (int i = 0; i < length_; ++i)
+    {
         letter_count.push_back(vector<int>(26, 0));
     }
 
-    for (string_view word : candidates_) {
+    for (const auto &candidate : candidates_)
+    {
+        string_view word = candidate.first;
         int index = 0;
-        for (const auto& chr : word) {
+        for (const auto &chr : word)
+        {
             ++letter_count[index++][chr - 'a'];
         }
     }
-    
+
     int best_score = 0;
     string best_guess;
-    for (string_view word : candidates_) {
+    for (const auto &candidate : candidates_)
+    {
+        bool is_candidate = candidate.second;
+        if (!is_candidate)
+        {
+            continue;
+        }
+        string_view word = candidate.first;
         vector<bool> unique_letters(26, false);
 
         int curr_score = 0;
         int index = 0;
-        for (const auto& chr : word) {
+        for (const auto &chr : word)
+        {
             int letter_index = chr - 'a';
-            if (unique_letters[letter_index]) {
+            if (unique_letters[letter_index])
+            {
                 continue;
             }
             curr_score += letter_count[index][letter_index];
             unique_letters[letter_index] = true;
         }
-        if (curr_score > best_score) {
+        if (curr_score > best_score)
+        {
             best_score = curr_score;
             best_guess = word;
         }
@@ -187,16 +234,28 @@ string Wordle::GetBestGuess() {
     return best_guess;
 }
 
-void Wordle::SolveOnce() {
-    PurgeCandidates();
-    cout << "Currently " << candidates_.size() << " candidates." << endl;
-    if (candidates_.size() <= kCandidateSizeToPrintAll) {
+void Wordle::SolveOnce()
+{
+    UpdateCandidates();
+    int candidate_size = count_if(candidates_.begin(), candidates_.end(), [](const pair<string, bool> &candidate)
+                                  { return candidate.second; });
+    cout << "Currently " << candidate_size << " candidates." << endl;
+    if (candidate_size <= kCandidateSizeToPrintAll)
+    {
         cout << "All candidates: ";
-        for (string_view candidate : candidates_) {
-            cout << candidate << " ";
+        for (const auto &candidate : candidates_)
+        {
+            bool is_candidate = candidate.second;
+            string_view word = candidate.first;
+            if (is_candidate)
+            {
+                cout << word << " ";
+            }
         }
         cout << endl;
-    } else { 
+    }
+    else
+    {
         cout << "Best guess: " << GetBestGuess() << endl;
     }
 }
--- src/wordle.h
+++ src/wordle.h
@@ -4,47 +4,75 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
+// Constraints from the current guesses.
 struct Constraints {
-    vector<int> letters;
-    unordered_set<char> must_contain;
+    // A vector of size `length_` (5 currently). Each contained int is a bitmask of currently
+    // acceptable letters for that letter location.
+    // That is, the i-th bit of letters[j = 1 <=> the j-th character of the answer cannot be
+    // the i-th letter of the alphabet.
+    //   e.g., if the vector is <0_2, 1_2, 10_2, 100_2, 1000_2>, it means the
+    //           - 1st letter has no restrictions
+    //           - 2nd letter cannot be 'a'
+    //           - 3rd letter cannot be 'b'
+    //           - 4th letter cannot be 'c'
+    //           - 5th letter cannot be 'd'
+    std::vector<int> letters;
+    // The set of letters the answer MUST contain (used to deal with yellow results).
+    std::unordered_set<char> must_contain;
 };
 
+// State corresponding to result for each letter.
 enum class State {
-    kNone = 0,
-    kAppears = 1,
-    kCorrect = 2,
+    kNone = 0,  // gray in UI
+    kAppears = 1,  // yellow in UI
+    kCorrect = 2,  // green in UI
 };
 
 class Wordle {
-    struct WordFormat {
-
-    };
-    
     public:
         Wordle(int word_length);
 
+        // Solves the current game, with user input.
+        // 1. Logs the "best" guess.
+        //    e.g. $ Best guess: "carbs"
+        // 2. Prompts the user for the guess the made, and the result from the UI. The result
+        //    is a sequence of 5 characters, corresponding to State enum values.
+        //    e.g. $ Input guess: "carbs"
+        //         $ Input result: "21100"
+        //    Implies "C" is green, "A" and "B" are yellow, "B" and "S" are gray.
+        // 3. Uses the given `result` to update `constraints_`. Uses the updated `constraints`
+        //    to update whether words in `candidates_` can still be valid solutions (but does
+        //    not prevent them from being guessed).
+        // 4. Repeat from step 1.
         void Solve();
 
     private:
+        // Performs one iteration; steps 1-3 above.
         void SolveOnce();
+        
+        // Checks if the given word fits `constraints`.
+        bool FitsConstraints(std::string_view word);
 
-        bool FitsConstraints(string_view word);
+        // Uses the `guess` and `result` to update `constraints_`.
+        void UpdateConstraints(std::string_view guess, string_view result);
 
-        void UpdateConstraints(string_view guess, string_view result);
+        // Uses `constraints` to modify whether a word in `candidates_` is still in the running
+        // as the answer.
+        void UpdateCandidates();
 
-        void PurgeCandidates();
-
+        // Scores all the words in `candidates_`, regardless of whether or not they can still be
+        // the answer, and returns the word with the highest score.
         string GetBestGuess();
 
-        bool ValidConstraints(string_view guess, string_view result);
+        // Checks if the user inputs for `guess` and `result` are syntactically correct, otherwise
+        // prompts the user again for these values.
+        bool ValidConstraints(std::string_view guess, std::string_view result);
 
 
         // Member variables.
         int length_;
 
-        unordered_set<string> candidates_;
+        std::unordered_map<string, bool> candidates_;
 
         Constraints constraints_;
 };
